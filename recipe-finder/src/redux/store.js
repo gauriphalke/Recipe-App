347657c29c108b40314f902dfe17316ac772d64d
Gauri Phalke <phalkegauri2001@gmail.com>
2025-01-09 22:02:01 +0530

    Update store.js

--- recipe-finder/src/redux/store.js
+++ recipe-finder/src/redux/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+// src/redux/store.js
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import recipeReducer from './recipeSlice';
-
-export const store = configureStore({
-    reducer: {
-        recipes: recipeReducer
+// Slice for recipes
+const recipeSlice = createSlice({
+    name: 'recipes',
+    initialState: {
+        savedRecipes: []
+    },
+    reducers: {
+        saveRecipe: (state, action) => {
+            if (!state.savedRecipes.includes(action.payload)) {
+                state.savedRecipes.push(action.payload);
+            }
+        },
+        removeRecipe: (state, action) => {
+            state.savedRecipes = state.savedRecipes.filter(
+                (recipe) => recipe !== action.payload
+            );
+        }
     }
 });
+
+const store = configureStore({
+    reducer: {
+        recipes: recipeReducer,
+    }
+});
+
+export const { saveRecipe, removeRecipe } = recipeSlice.actions;
+export const selectSavedRecipes = (state) => state.recipes.savedRecipes;
+
+
+
+
+export default store;
